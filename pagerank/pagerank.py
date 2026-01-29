33a6b455621770fbac2741e19aad8fec17e3ca1e
nilibungus <109638176+nilibungus@users.noreply.github.com>
2026-01-29 15:30:36 +0100

    Update pagerank.py

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -57,19 +57,53 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+
+
 
 
 def sample_pagerank(corpus, damping_factor, n):
-    """
-    Return PageRank values for each page by sampling `n` pages
-    according to transition model, starting with a page at random.
+    #make a dictionary to count the pages, with keys the same keys as in the corpus dictionary:
+    page_counter = dict()
+    for page in corpus:
+        page_counter[page] = 0
+    #make a list out of corpus over which we can iterate and randomise:
+    corpus_list = list(corpus.keys())
+    #obtain the first page at random from the keys of corpus:
+    random_index = random.randrange(len(corpus_list))
 
-    Return a dictionary where keys are page names, and values are
-    their estimated PageRank value (a value between 0 and 1). All
-    PageRank values should sum to 1.
-    """
-    raise NotImplementedError
+    new_page = corpus_list[random_index]
+
+
+    #iterate n times, to obtain n samples:
+    k = 0
+    while k < n:
+        rando = random.randint(0, 100)
+        if rando <= damping_factor*100 and list(corpus[new_page]):
+
+                new_page= random.choice(list(corpus[new_page]))
+
+
+        else:
+            new_page = random.choice(corpus_list)
+        page_counter[new_page] += 1
+        k += 1
+
+    #make the dictionary of page ranks:
+
+    pageranks_dic = dict()
+
+    for key in page_counter:
+        pageranks_dic[key] = page_counter[key]/n
+
+    return pageranks_dic
+
+
+
+
+
+
+
+
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -81,7 +115,47 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+
+
+    #calculate the number of pages
+    n = len(list(corpus.keys()))
+    #create a dictionary with keys being the pages in corpus and th values being a list with each PR at each time step:
+    pagerank_dictionary = dict()
+
+    for key in corpus:
+        pagerank_dictionary[key] = [1/n]
+    #check convergence:
+    converge = False
+
+
+    while not converge:
+        new_values = dict()
+        dangling_sum = sum(pagerank_dictionary[p][-1]/n for p in corpus if not corpus[p])
+        for key in pagerank_dictionary:
+            linking_pages = []
+            for page in corpus:
+                if key in corpus[page]:
+                    linking_pages.append(page)
+
+            sigma = 0
+
+            for page in linking_pages:
+                if corpus[page]:
+
+                    sigma += pagerank_dictionary[page][-1]/ len(corpus[page])
+
+
+            new_value = ((1 - damping_factor)/n) + damping_factor*(sigma + dangling_sum)
+
+
+            new_values[key] = new_value
+        for page in pagerank_dictionary:
+            pagerank_dictionary[page].append(new_values[page])
+
+
+        converge = all(abs(pagerank_dictionary[page][-1] - pagerank_dictionary[page][-2]) < 0.001 for page in corpus)
+    return {p: pagerank_dictionary[p][-1] for p in pagerank_dictionary}
+
 
 
 if __name__ == "__main__":
